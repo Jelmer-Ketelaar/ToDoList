2a60ebaa0c1bd52d4ad674442b6093136f252acb
Jelmer Ketelaar <99053633@mydavinci.nl>
2021-02-24 23:12:00 +0100

    [TASK] Added al the code to delete a list

--- CRUD_list/deleteList.php
+++ CRUD_list/deleteList.php
@@ -1,11 +1,12 @@
 <?php
-include "include/config.php";
+session_start();
 //Check whether the list_id is assigned or not
 if (isset($_GET['list_id'])) {
     //Delete the list from the database
 
     //Get the last ID Value From URL or Get Method
     $list_id = $_GET['list_id'];
+    $task_id = $_GET['task_id'];
 
     //Database connection
     $conn = mysqli_connect('localhost', 'root', 'mysql');
@@ -13,31 +14,29 @@ if (isset($_GET['list_id'])) {
     //Select Database
     $db_select = mysqli_select_db($conn, 'ToDoList');
 
-    //Select all from list
+
+    //Delete from list where id = $list_id
     $sql = "DELETE FROM ToDoList.list WHERE list_id = $list_id";
+        //Execute the query
+        $result = mysqli_query($conn, $sql);
 
-    //Execute the query
-    $result = mysqli_query($conn, $sql);
+        //Check whether the query executed successfully or not
+        if ($result == true) {
+            //Query Executed Successfully, wich means that the list is deleted successfully
+            $_SESSION['delete'] = 'List deleted Successfully';
 
-    //Check whether the query executed successfully or not
-    if($result == true) {
-        //Query Executed Successfully, wich means that the list is deleted successfully
-        $_SESSION['delete'] = 'List deleted Successfully';
+            //Redirect to manage list page
+            header('location:' . 'http://localhost/Jaar-2/Blok-1/ToDoList/manageList.php');
+        } else {
+            //Failed to delete the list
+            //Query Executed Successfully, wich means that the list is deleted successfully
+            $_SESSION['delete_fail'] = 'Failed to delete the list';
 
-        //Redirect to manage list page
-        header('location:'. 'http://localhost/Jaar-2/Blok-1/ToDoList/manageList.php');
-    } else {
-        //Failed to delete the list
-        //Query Executed Successfully, wich means that the list is deleted successfully
-        $_SESSION['delete_fail'] = 'Failed to delete the list';
+            //Redirect to manage list page
+            header('location:' . 'http://localhost/Jaar-2/Blok-1/ToDoList/manageList.php');
 
-        //Redirect to manage list page
-        header('location:'. 'http://localhost/Jaar-2/Blok-1/ToDoList/manageList.php');
+        }
 
     }
 
-} else {
-    //Redirect To Manage Lists Page
-    header("location:" . "http://localhost/Jaar-2/Blok-1/ToDoList/manageList.php");
-}
 
