0c7ef9398c84960203e621000a09636043e16b54
Jelmer Ketelaar <99053633@mydavinci.nl>
2021-02-24 23:10:15 +0100

    [TASK] Made the table where you can see al the added tasks

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include "include/config.php";
+session_start();
 ?>
 
 <!doctype html>
@@ -28,63 +28,181 @@ include "include/config.php";
 <body>
 <!-- Navbar starts here -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">Home Page</a>
+    <a class="navbar-brand" href="#" style="cursor: auto">Home Page</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">To-Do <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Doing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Done</a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href="manageList.php">Manage Lists</a>
             </li>
+
+            <?php
+            //Connect to database
+            $conn2 = mysqli_connect('localhost', 'root', 'mysql');
+
+            //Query to get the lists from the database
+            $sql2 = "SELECT * FROM ToDoList.list";
+
+            //Execute the query
+            $result2 = mysqli_query($conn2, $sql2);
+
+            //Check whether the query executed or not
+            if ($result2 == true) {
+                //Display the list in menu
+                while ($row2 = mysqli_fetch_assoc($result2)) {
+                    $list_id = $row2['list_id'];
+                    $list_name = $row2['list_name'];
+                    ?>
+
+                    <li class="nav-item ">
+                        <a class="nav-link" href="listTask.php?list_id=<?php echo $list_id;?>"><?php echo $list_name ?></a>
+                    </li>
+
+                    <?php
+                }
+            }
+            ?>
         </ul>
     </div>
 </nav>
-
 <!-- Navbar ends here -->
+
+
 <h1>ToDoList</h1>
 
-<!-- Tasks starts here -->
-<div class="task">
+<p>
+    <?php
+
+    //Checks whether the session is created or not
+    if (isset($_SESSION['add'])) {
+        //Displays session message
+        echo $_SESSION['add'];
+        //Removes the message after displaying once
+        unset($_SESSION['add']);
+    }
+
+    //Check whether the session is deleted or not
+    if (isset($_SESSION['delete'])) {
+        //Displays session message
+        echo($_SESSION['delete']);
+        //Removes the message after displaying once
+        unset($_SESSION['delete']);
+    }
+
+    if (isset($_SESSION['delete_fail'])) {
+        //Displays session message
+        echo $_SESSION['delete_fail'];
+        //Removes the message after displaying once
+        unset($_SESSION['delete_fail']);
+
+    }
+
+    //Check whether the session is set or not
+    if (isset($_SESSION['update'])) {
+
+        echo $_SESSION['update'];
+        unset ($_SESSION['update']);
+    }
+
+    ?>
+</p>
+
+
+<!--Table to display lists starts here-->
+
+<div class="all-lists">
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-hover">
             <thead>
             <tr>
                 <th scope="row">ID</th>
                 <th>Task Name</th>
+                <th>Task Description</th>
                 <th>Priority</th>
                 <th>Begin Date</th>
                 <th>Deadline</th>
                 <th>Actions</th>
             </tr>
             </thead>
+
+
+            <?php
+            //Database connection
+            $conn = mysqli_connect('localhost', 'root', 'mysql');
+            //Returns the error code from last connect call
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                exit();
+            }
+
+            //Select Database
+            $db_select = mysqli_select_db($conn, 'ToDoList');
+
+            //Select all from list
+            $sql = "SELECT * FROM ToDoList.task";
+
+            //Execute The Query
+            $result = mysqli_query($conn, $sql);
+
+            //Checks whether the query is executed successfully or not
+            if ($result == true) {
+            //Count rows of data in  database
+            $count_rows = mysqli_num_rows($result);
+
+            $id = 1;
+
+            //Check whether there is data in database or not
+            if ($count_rows > 0) {
+
+            //If There is data in the database. Display in table
+            //$row is an array. The data will be stored as an array in $row
+            while ($row = mysqli_fetch_assoc($result)) {
+            $task_id = $row['task_id'];
+            $task_name = $row['task_name'];
+            $task_description = $row['task_description'];
+            $priority = $row['priority'];
+            $begin_date = $row['begin_date'];
+            $deadline = $row['deadline'];
+
+            ?>
             <tbody>
+
+            <!-- Row -->
             <tr>
-                <th scope="row">1.</th>
-                <td>Design a website</td>
-                <td>Medium</td>
-                <td>19-02-2021</td>
-                <td>25-02-2021</td>
+                <!-- Used ID++ because else when I delete a task/list, it will not count from the last existing ID -->
+                <th scope="row"><?php echo $id++ ?></th>
+                <td><?php echo $task_name ?></td>
+                <td><?php echo $task_description ?></td>
+                <td><?php echo $priority ?></td>
+                <td><?php echo $begin_date ?></td>
+                <td><?php echo $deadline ?></td>
                 <td>
-                    <a href="#"> Update </a>
-                    <a href="#"> Delete </a>
+                    <a href="CRUD_task/updateTask.php?task_id=<?php echo $task_id; ?>">Update</a>
+                    <a href="CRUD_task/deleteTask.php?task_id=<?php echo $task_id; ?>">Delete</a>
                 </td>
             </tr>
+
+            <?php
+            }
+            } else {
+                //Else Show a Message, That There Is No Data In The Database
+                ?>
+                <!-- Makes 1 column out of different columns -->
+                <tr>
+                    <td colspan="3">No List Added Yet.</td>
+                </tr>
+
+                <?php
+            }
+            }
+            ?>
             </tbody>
         </table>
     </div>
 </div>
 <a class="btn btn-primary" href="CRUD_task/addTask.php">Add Task</a>
-<!-- Tasks starts here -->
 </body>
 </html>
